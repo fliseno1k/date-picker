9a001070b7f5bddfe87ac7ad2fee9c751c37e1a7
Alexey <fliseno1k@gmail.com>
2021-07-22 13:54:08 +1000

    add date property

--- src/engine/Item.ts
+++ src/engine/Item.ts
@@ -14,6 +14,7 @@ export class Item {
     constructor(
         public readonly id: string, 
         public readonly value: number | string, 
+        public readonly date: Date,
         public readonly types: string[] = [],
     ) {}
 
