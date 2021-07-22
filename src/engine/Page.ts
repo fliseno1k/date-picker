6593da20c0c4bd43771735181f04e1e4228d9f95
Alexey <fliseno1k@gmail.com>
2021-07-22 13:53:11 +1000

    Restruct page model

--- src/engine/Page.ts
+++ src/engine/Page.ts
@@ -1,9 +1,5 @@
 import { Item } from "./Item";
 
-export type PagePart = {
-    date:  Date;
-    items: Item[];
-}
 
 export type FormatingOptions = {
     rows: number, 
@@ -13,41 +9,19 @@ export type FormatingOptions = {
 export class Page {
 
     constructor(
-        private title            : string,
-        private current          : PagePart,
-        private previous         : PagePart, 
-        private next             : PagePart,
-        private options : FormatingOptions,
+        public date: Date,
+        public title: string,
+        public items: Item[],
+        private options: FormatingOptions,
     ) {}
 
-    public toArray() {
-        return [
-            ...this.previous.items, 
-            ...this.current.items, 
-            ...this.next.items
-        ];
-    }
-
     public toRows() {
-        const page = this.toArray();
         const rows: Item[][] = [];
 
-        for (let i = 0, j = page.length; i < j; i += this.options.cols) {
-            rows.push(page.slice(i, i + this.options.cols));
+        for (let i = 0, j = this.items.length; i < j; i += this.options.cols) {
+            rows.push(this.items.slice(i, i + this.options.cols));
         }
 
         return rows;
-    }
-
-    get month() {
-        return this.current.date.getMonth();
-    }
-
-    get year() {
-        return this.current.date.getFullYear();
-    }
-
-    get pageTitle() {
-        return this.title;
     }
 }
