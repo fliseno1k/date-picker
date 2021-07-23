a7412f7eb2029c7d3e710604b2927e8a657211f0
fliseno1k <fliseno1k@gmail.com>
2021-07-23 18:38:19 +1000

    combine preview and calendar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,75 @@
 <template>
-  <div class="full-screen">
-    <date-picker />
+    <div class="full-screen">
+        <div
+            ref="datePicker"
+            class="date-picker"
+        >
+            <div class="date-picker__container">
+                <date-picker-preview
+                    :range="range"
+                    @togglePicker="onPickerToggle"
+                    @clearRange="onRangeClear"
+                />
+                <date-picker-calendar
+                    v-if="isPickerOpen"
+                    :style="calendarStyles"
+                    :range="range"
+                    @outsideClick="onOutsideClick"
+                    @rangeUpdate="onRangeUpdate"
+                />
+            </div>
+        </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import DatePicker from './components/date-picker.vue'
+import DatePickerCalendar from './components/date-picker-calendar.vue';
+import DatePickerPreview from './components/date-picker-preview.vue';
+import { DateRange } from "@/engine/date-range";
 
 @Options({
-  components: {
-    DatePicker,
-  },
+    components: {
+        DatePickerCalendar,
+        DatePickerPreview,
+    },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+    isPickerOpen = false;
+    calendarStyles = {};
+    dateRange = new DateRange();
+
+    mounted() {
+        const { right, width } = (this.$refs.datePicker as HTMLElement).getBoundingClientRect();
+        const property = right - width / 2 > window.innerWidth / 2 ? 'right' : 'left';
+
+        this.calendarStyles = {
+            position: 'absolute',
+            top: 'calc(100% + 10px)',
+            [property]: 0,
+        };
+    }
+
+    onPickerToggle() {
+        this.isPickerOpen = !this.isPickerOpen;
+    }
+
+    onOutsideClick() {
+        this.isPickerOpen = false;
+    }
+
+    onRangeUpdate(bound: any) {
+        this.dateRange.pushBound(bound);
+    }
+
+    onRangeClear() {
+        this.dateRange.clearBounds();
+    }
+
+    get range() {
+        return this.dateRange.orderedRange;
+    }
+}
 </script>
 <style>
 
@@ -37,7 +93,12 @@ export default class App extends Vue {}
   overflow: hidden;
 
   display: flex;
-  align-items: center;
-  justify-content: center;
+  align-items: flex-start;
+  justify-content: flex-start;
+    padding: 100px;
+}
+
+.date-picker {
+    position: relative;
 }
 </style>
